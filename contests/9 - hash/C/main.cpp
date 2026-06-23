d4d8d6e4d86154cab4f37f3b918aa24c79dcd94e
Elizabeth <veta.gorbunova372007@gmail.com>
2026-06-23 15:31:57 +0300

    smth

---
+++ contests/9 - hash/C/main.cpp
@@ -0,0 +1,21 @@
+//
+// Created by 1 on 22.06.2026.
+//
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        int p;
+        char c;
+        cin >> p >> c;
+
+    }
+    return 0;
+}
